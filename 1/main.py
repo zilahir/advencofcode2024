f22dce73748bcbd89c0762c0c0934a73ee62d759
Richard Zilahi <zilahi@gmail.com>
2024-12-01 19:34:39 +0200

    part2

--- 1/main.py
+++ 1/main.py
@@ -17,27 +17,41 @@ def main():
                 arr1.append(colA)
                 arr2.append(colB)
 
-    distance = []
+    # distance = []
+
+    similartyscores = []
 
     if len(arr1) != len(arr2):
         print("Arrays are not of same length")
         return
 
+    # for i in range(len(arr1)):
+    #     sm1 = min(arr1)
+    #     sm2 = min(arr2)
+    #
+    #     distance.append(abs(sm1 - sm2))
+    #
+    #     arr1.remove(sm1)
+    #     arr2.remove(sm2)
+    #
+    # # print the distance array
+    # sum_distance = 0
+    # for i in range(len(distance)):
+    #     sum_distance += distance[i]
+    #
+    # print("Sum of distances between the two arrays is: ", sum_distance)
+
     for i in range(len(arr1)):
-        sm1 = min(arr1)
-        sm2 = min(arr2)
+        this_number = arr1[i]
+        # count the number of times this_number appears in arr2
+        count = arr2.count(this_number)
+        similartyscores.append(this_number * count)
 
-        distance.append(abs(sm1 - sm2))
+    sum_similarity = 0
+    for i in range(len(similartyscores)):
+        sum_similarity += similartyscores[i]
 
-        arr1.remove(sm1)
-        arr2.remove(sm2)
-
-    # print the distance array
-    sum_distance = 0
-    for i in range(len(distance)):
-        sum_distance += distance[i]
-
-    print("Sum of distances between the two arrays is: ", sum_distance)
+    print("Sum of similarity scores between the two arrays is: ", sum_similarity)
 
 
 if __name__ == "__main__":
