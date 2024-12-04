3e5422ee2d0f815fd4d021dd34072dbe696601bf
Richard Zilahi <zilahi@gmail.com>
2024-12-04 22:08:17 +0200

    day4 p2

--- 4/main.py
+++ 4/main.py
@@ -1,52 +1,60 @@
-def isValid(x, y, sizeX, sizeY):
-    return 0 <= x < sizeX and 0 <= y < sizeY
+def isValid(x, y, n, m):
+    return 0 <= x < n and 0 <= y < m
 
 
-def findWordInDirection(grid, n, m, word, index, x, y, dirX, dirY):
-    if index == len(word):
-        return True
+def checkDiagonal(grid, x, y, dx1, dy1, dx2, dy2, n, m):
+    """
+    Check if two diagonals form "MAS" or "SAM" relative to the center.
+    """
+    # First diagonal
+    x1, y1 = x + dx1, y + dy1
+    x2, y2 = x - dx1, y - dy1
 
-    if isValid(x, y, n, m) and word[index] == grid[x][y]:
-        return findWordInDirection(
-            grid, n, m, word, index + 1, x + dirX, y + dirY, dirX, dirY
-        )
+    # Second diagonal
+    x3, y3 = x + dx2, y + dy2
+    x4, y4 = x - dx2, y - dy2
 
+    # Ensure all positions are within bounds
+    if all(
+        isValid(xi, yi, n, m) for xi, yi in [(x1, y1), (x2, y2), (x3, y3), (x4, y4)]
+    ):
+        diagonal1 = grid[x1][y1] + grid[x][y] + grid[x2][y2]
+        diagonal2 = grid[x3][y3] + grid[x][y] + grid[x4][y4]
+
+        # Check if each diagonal contains "MAS" or "SAM"
+        return sorted(diagonal1) == ["A", "M", "S"] and sorted(diagonal2) == [
+            "A",
+            "M",
+            "S",
+        ]
     return False
 
 
-def searchWord(grid, word):
-    ans = []
+def countXMASPatterns(grid):
     n = len(grid)
     m = len(grid[0])
+    count = 0
 
-    # Directions for all possible movements
-    directions = [(1, 0), (-1, 0), (0, 1), (0, -1), (1, 1), (1, -1), (-1, 1), (-1, -1)]
+    diagonals = [
+        ((1, 1), (1, -1)),  # Top-left to bottom-right, Top-right to bottom-left
+    ]
 
-    for i in range(n):
-        for j in range(m):
-            # Check if the first character matches
-            if grid[i][j] == word[0]:
-                for dirX, dirY in directions:
-                    if findWordInDirection(grid, n, m, word, 0, i, j, dirX, dirY):
-                        ans.append([i, j])
+    for x in range(n):
+        for y in range(m):
+            if grid[x][y] == "A":  # Center of an X
+                for (dx1, dy1), (dx2, dy2) in diagonals:
+                    if checkDiagonal(grid, x, y, dx1, dy1, dx2, dy2, n, m):
+                        count += 1
 
-    return ans
+    return count
 
 
 def main():
     with open("input.txt", "r") as file:
-        # read input
-        input = file.read().strip()
-        # close input file
-        file.close()
+        grid = [list(line.strip()) for line in file.readlines()]
 
-    grid = []
-    word = "XMAS"
-    for line in input.split("\n"):
-        grid.append(list(line.strip()))
-
-    ans = searchWord(grid, word)
-    print(len(ans))
+    result = countXMASPatterns(grid)
+    print(result)
 
 
 if __name__ == "__main__":
