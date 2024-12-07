8a492469d7ed2b5ce99a5bb2a5bc34ac1c7ff32b
Richard Zilahi <zilahi@gmail.com>
2024-12-07 16:07:46 +0200

    day7 p2

--- 7/main.py
+++ 7/main.py
@@ -2,21 +2,21 @@ from itertools import product
 
 
 def evaluate_left_to_right(numbers, operators):
-    """Evaluate the expression left-to-right given numbers and operators."""
     result = numbers[0]
     for i in range(len(operators)):
         if operators[i] == "+":
             result += numbers[i + 1]
         elif operators[i] == "*":
             result *= numbers[i + 1]
+        elif operators[i] == "||":  # Concatenate numbers
+            result = int(str(result) + str(numbers[i + 1]))
     return result
 
 
 def solve_line(target, numbers):
-    """Check if any combination of operators results in the target value."""
     num_count = len(numbers)
-    # Generate all possible operator combinations: '+' or '*'
-    for operators in product(["+", "*"], repeat=num_count - 1):
+    # Generate all possible operator combinations: '+', '*', or '||'
+    for operators in product(["+", "*", "||"], repeat=num_count - 1):
         result = evaluate_left_to_right(numbers, operators)
         if result == target:
             return True  # Found a valid equation
